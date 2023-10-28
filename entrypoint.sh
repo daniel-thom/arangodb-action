b1b8b8f9f73021c55b69178ec7988cbba0d0f056
Daniel Thom <daniel.thom@nrel.gov>
2023-10-28 10:55:33 -0600

    Add container name
    This allows subsequent steps in the workflow to connect to the database
    with ArangoDB tools.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,13 @@
 # so we have to run this via a regular command and lose out on some configurability.
 CMD="arangodb --starter.mode=single --starter.address=0.0.0.0"
 
-docker_run="docker run"
-docker_run="$docker_run -d -p 8529:8529 arangodb:$INPUT_ARANGODB_VERSION $CMD"
+if [[ -z $INPUT_BIND_MOUNT_TARGET_DIR ]]; then
+    BIND_MOUNT=""
+else
+    BIND_MOUNT="-v $GITHUB_WORKSPACE:$INPUT_BIND_MOUNT_TARGET_DIR"
+fi
 
+docker_run="docker run $BIND_MOUNT --name=$INPUT_CONTAINER_NAME -d -p 8529:8529 arangodb:$INPUT_ARANGODB_VERSION $CMD"
+
+echo "running docker command: $docker_run -- current directory = $(pwd) - bind_mount=$BIND_MOUNT INPUT_BIND_MOUNT_TARGET_DIR=$INPUT_BIND_MOUNT_TARGET_DIR gh-workspace=$GITHUB_WORKSPACE"
 sh -c "$docker_run"
