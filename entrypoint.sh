098c2029c00121ed1f66703e63dee25448c2617a
Daniel Thom <daniel.thom@nrel.gov>
2023-10-28 10:55:33 -0600

    Add container name
    This allows subsequent steps in the workflow to run commands, like foxx,
    in the running container.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,6 @@
 # so we have to run this via a regular command and lose out on some configurability.
 CMD="arangodb --starter.mode=single --starter.address=0.0.0.0"
 
-docker_run="docker run"
-docker_run="$docker_run -d -p 8529:8529 arangodb:$INPUT_ARANGODB_VERSION $CMD"
+docker_run="docker run --name=$INPUT_CONTAINER_NAME -d -p 8529:8529 arangodb:$INPUT_ARANGODB_VERSION $CMD"
 
 sh -c "$docker_run"
